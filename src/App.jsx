b676435078ff28d3b94b76f03d9bd66e778e75c6
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2025-10-06 16:54:43 -0400

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './sections/Projects'
 import Experience from './sections/Experience'
 import Contact from './sections/Contact'
 import Footer from './sections/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
       <Experience />
       <Contact />
       <Footer />
-
+      <Analytics />
     </div>
   )
 }
